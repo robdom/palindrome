b997ff86ecac515b8813de62a0fd43ac6fd38764
Robert Domingo <robertdomingo@protonmail.com>
2021-05-24 13:31:38 -0400

    Add tests for palindrome with punctuation

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ function Phrase(content) {
     return this.content.toLowerCase();
   }
 
+  // Returns the letters in the content.
+  this.letters = function letters() {
+    return this.content; // stub return value
+  }
+
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
     return this.processedContent() === this.processedContent().reverse();
--- test/test.js
+++ test/test.js
@@ -20,6 +20,16 @@ describe("Phrase", function() {
       assert(mixedCase.palindrome());
     });
 
-    it("should return true for a palindrome with punctuation");
+    it("should return true for a palindrome with punctuation", function() {
+      let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+      assert(punctuatedPalindrome.palindrome());
+    });
+  });
+
+  describe("#letters", function() {
+    it("should return only letters", function() {
+      let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+      assert.strictEqual(punctuatedPalindrome.letters(), "MadamImAdam");
+    });
   });
 });
